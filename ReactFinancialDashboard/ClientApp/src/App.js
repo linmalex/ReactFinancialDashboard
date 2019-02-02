e615674b42743580151dc8b94e6e747d9676a15d
Lindsay Alexander <linmalex@gmail.com>
2019-02-02 15:49:12 -0800

    delete transactions to focus on account level access first

--- ReactFinancialDashboard/ClientApp/src/App.js
+++ ReactFinancialDashboard/ClientApp/src/App.js
@@ -4,7 +4,6 @@ import { Layout } from "./components/Layout";
 import { Home } from "./components/Home";
 import { FetchData } from "./components/FetchData";
 import ListOfBills from "./components/ListOfBills";
-import YnabTransactions from "./components/YnabTransactions";
 
 export default class App extends Component {
   displayName = App.name;
@@ -15,7 +14,6 @@ export default class App extends Component {
         <Route exact path="/" component={Home} />
         <Route path="/fetchdata" component={FetchData} />
         <Route path="/listofbills" component={ListOfBills} />
-        <Route path="/ynabtransactions" component={YnabTransactions} />
       </Layout>
     );
   }
