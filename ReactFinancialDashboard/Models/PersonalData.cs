9099b75805aadb46e445b3726a4d04bf226342d3
Lindsay Alexander <linmalex@gmail.com>
2019-02-01 17:00:26 -0800

    Begin work to get YNAB transaction data

---
+++ ReactFinancialDashboard/Models/PersonalData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReactFinancialDashboard.Models
+{
+    public class PersonalData
+    {
+        public Dictionary<string, double> InterestRatesDictionary { get; set; }
+        public Dictionary<string, string> AccountsIdDictionary { get; set; }
+        public Dictionary<string, PayoffPriority> PIFGoalStatus { get; set; }
+        public DateTime PayOffDate { get; set; }
+        public string BudgetID { get; set; }
+        public string AuthToken { get; set; }
+        public double MonthlyIncomeSource1 { get; set; }
+        public double MonthlyIncomeSource2 { get; set; }
+
+
+    }
+}
