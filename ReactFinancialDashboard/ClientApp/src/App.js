3ea054b11f5bfc4ca6f65147eefcdb4255c23964
Lindsay Alexander <linmalex@gmail.com>
2019-02-02 14:56:44 -0800

    autoformatting

--- ReactFinancialDashboard/ClientApp/src/App.js
+++ ReactFinancialDashboard/ClientApp/src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import ListOfBills from './components/ListOfBills';
-import { YnabTransactions } from './components/YnabTransactions';
-
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import ListOfBills from "./components/ListOfBills";
+import { YnabTransactions } from "./components/YnabTransactions";
 
 export default class App extends Component {
-    displayName = App.name
+  displayName = App.name;
 
-    render() {
-        return (
-            <Layout>
-                <Route exact path='/' component={Home} />
-                <Route path='/fetchdata' component={FetchData} />
-                <Route path='/listofbills' component={ListOfBills} />
-                <Route path='/ynabtransactions' component={YnabTransactions} />
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <Route path="/fetchdata" component={FetchData} />
+        <Route path="/listofbills" component={ListOfBills} />
+        <Route path="/ynabtransactions" component={YnabTransactions} />
+      </Layout>
+    );
+  }
 }
