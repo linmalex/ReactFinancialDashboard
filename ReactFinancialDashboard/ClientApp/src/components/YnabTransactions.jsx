401e1573ba24d4e13d63def32da015aefb98fceb
Lindsay Alexander <linmalex@gmail.com>
2019-02-02 14:58:21 -0800

    tweak formatting

--- ReactFinancialDashboard/ClientApp/src/components/YnabTransactions.jsx
+++ ReactFinancialDashboard/ClientApp/src/components/YnabTransactions.jsx
@@ -1,56 +1,58 @@
-﻿import React, { Component } from 'react';
-import { YnabTransactions } from './YnabTransaction';
+﻿import React, { Component } from "react";
 
-class YnabTransaction extends Component {
-    static renderTransactionsTable(transactions) {
-        return (
-            <table className='table'>
-                <thead>
-                    <tr>
-                        <th scope="col">Pay To</th>
-                        <th scope="col">Category</th>
-                        <th scope="col">Amount</th>
-                        <th scope="col">Due</th>
-                        <th scope="col">Memo</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {transactions.map(transaction =>
-                        (<tr key={transaction.id}>
-                            <td>{transaction.payee_name}</td>
-                            <td>{transaction.category_name}</td>
-                            <td>{transaction.amount}</td>
-                            <td>{transaction.date}</td>
-                            <td>{transaction.Memo}</td>
-                        </tr>)
-                    )}
-                </tbody>
-            </table>
-        );
-    }
-    constructor(props) {
-        super(props);
-        this.state = { transactions: [], loading: true };
+class YnabTransactions extends Component {
+  static renderTransactionsTable(transactions) {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Pay To</th>
+            <th scope="col">Category</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Due</th>
+            <th scope="col">Memo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.payee_name}</td>
+              <td>{transaction.category_name}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.date}</td>
+              <td>{transaction.Memo}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+  constructor(props) {
+    super(props);
+    this.state = { transactions: [], loading: true };
 
-        fetch('api/Transaction/GetYnabTransactions')
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ transactions: data, loading: false });
-            });
-    }
+    fetch("api/Transaction/GetYnabTransactions")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ transactions: data, loading: false });
+      });
+  }
 
-    render() {
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : this.renderTransactionsTable(this.state.transactions);
+  render() {
+    let contents = this.state.loading ? (
+      <p>
+        <em>Loading...</em>
+      </p>
+    ) : (
+      this.renderTransactionsTable(this.state.transactions)
+    );
 
-        return (
-            <div>
-                <h1>Transactions</h1>
-                {contents}
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h1>Transactions</h1>
+        {contents}
+      </div>
+    );
+  }
 }
 export default YnabTransactions;
-
