1b37280b3c12471d8e5e5e243cd176c9f01faed0
Lindsay Alexander <linmalex@gmail.com>
2019-02-06 10:31:44 -0800

    Begin work to add unit testing

---
+++ NunitTest/Tests/YNABCreditCardControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using ReactFinancialDashboard.Controllers;
+using ReactFinancialDashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NunitTest.Tests
+{
+    [TestFixture]
+    class YNABCreditCardControllerTests
+    {
+        [Test]
+        public void CreditCardCreation()
+        {
+            CreditCard x = new CreditCard();
+            
+        }
+    }
+}
