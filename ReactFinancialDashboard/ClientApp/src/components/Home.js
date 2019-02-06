639e29153bae46fc1cdc44590bb42ffa4c1787f4
Lindsay Alexander <linmalex@gmail.com>
2019-02-05 18:59:17 -0800

    Home populating with YNAB data

--- ReactFinancialDashboard/ClientApp/src/components/Home.js
+++ ReactFinancialDashboard/ClientApp/src/components/Home.js
@@ -1,28 +1,65 @@
 import React, { Component } from "react";
 
 export class Home extends Component {
-  displayName = Home.name;
+    static renderAccountsTable(accounts) {
+        return (
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Account Name</th>
+                        <th>Account Balance</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {accounts.map(account =>
+                        <tr key={account.id}>
+                            <td>{account.name}</td>
+                            <td>{account.balance}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
 
-  state = {
-    accounts: []
-  };
+    constructor(props) {
+        super(props);
+        this.state = { accounts: [], loading: true };
 
-  getAccounts = () => {
-    fetch("api/YnabAccount/YNABAccountsJson")
-      .then(response => response.json())
-      .then(data => {
-        var results = data["data"]["accounts"];
-        console.log(typeof results);
-        var accounts = [];
-        for (var x in results) {
-          accounts.push(typeof x);
-        }
-        console.log(accounts);
-      });
-  };
+        this.getYnabAccountsData();
+    }
 
-  render() {
-    this.getAccounts();
-    return <div>{this.state.accounts}</div>;
-  }
+    getYnabAccountsData = () => {
+        fetch('api/YnabAccount/YNABAccountsJson')
+            .then(response => response.json())
+            .then(data => {
+                var x = data["data"]["accounts"];
+                console.log(x);
+                this.setState({ accounts: x, loading: false });
+            });
+    }
+
+    getStatementData = () => {
+        fetch('api/YnabAccount/YNABAccountsJson')
+            .then(response => response.json())
+            .then(data => {
+                var x = data["data"]["accounts"];
+                console.log(x);
+                this.setState({ accounts: x, loading: false });
+            });
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Home.renderAccountsTable(this.state.accounts);
+
+        return (
+            <div>
+                <h1>Credit Cards</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
 }
