2c82f954e299da2160f006f3b574b977e95ea8f4
Lindsay Alexander <linmalex@gmail.com>
2019-02-15 17:37:53 -0800

    Add base view models

---
+++ ReactFinancialDashboard/ViewModels/LoadingData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactFinancialDashboard.ViewModels
+{
+    public class LoadingData
+    {
+        public string PageTitle { get; set; }
+        public bool DataLoading { get; set; }
+    }
+}
