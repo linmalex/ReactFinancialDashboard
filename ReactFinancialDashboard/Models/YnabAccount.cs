468f9466f86c3d94b064e34e80334f084dac6358
Lindsay Alexander <linmalex@gmail.com>
2019-02-22 15:46:22 -0800

    add error handling to YnabAccount when requesting data from API

--- ReactFinancialDashboard/Models/YnabAccount.cs
+++ ReactFinancialDashboard/Models/YnabAccount.cs
@@ -2,29 +2,34 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using ReactFinancialDashboard.Data;
+using ReactFinancialDashboard.Data.Utilities;
 
-namespace ReactFinancialDashboard.Models {
-    public class YnabAccount {
+namespace ReactFinancialDashboard.Models
+{
+    public class YnabAccount
+    {
         #region Properties
         public string ID { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
 
-        [DataType (DataType.Currency)]
+        [DataType(DataType.Currency)]
         public double Balance { get; set; }
 
-        [DataType (DataType.Currency)]
+        [DataType(DataType.Currency)]
         public double Cleared_balance { get; set; }
         public string Note { get; set; }
 
-        [DataType (DataType.Currency)]
+        [DataType(DataType.Currency)]
         public double Uncleared_balance { get; set; }
         public string Transfer_payee_id { get; set; }
         public bool Deleted { get; set; }
@@ -41,32 +46,45 @@ namespace ReactFinancialDashboard.Models {
         /// </summary>
         /// <param name="personalData"></param>
         /// <returns>JObject containing new API data</returns>
-        public static async Task<JObject> GetAPIYnabAccountsJSON (PersonalData personalData) {
-            HttpClient client = new HttpClient ();
-            string uri = "https://api.youneedabudget.com/v1/budgets/" + personalData.BudgetID + "/accounts";
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue ("Bearer", personalData.AuthToken);
-            JObject transactionData = JObject.Parse (await client.GetStringAsync (uri));
-            return transactionData;
+        public static async Task<JObject> GetAPIYnabAccountsJSON(PersonalData personalData)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string uristring = "https://api.youneedabudget.com/v1/budgets/" + personalData.BudgetID + "/accounts";
+                var uri = new Uri(uristring);
+                client.BaseAddress = uri;
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", personalData.AuthToken);
+                string response = await client.GetStringAsync(uri);
+                JObject transactionData = JObject.Parse(response);
+                return transactionData;
+            }
+            catch (Exception e)
+            {
+                return ErrorMessageWriter.ExceptionToJObjectWriter(e);
+            }
         }
-        
+
         /// <summary>
         /// Gets new YNAB data as JSON and converts it to List
         /// </summary>
         /// <param name="context"></param>
         /// <param name="personalBudgetID"></param>
         /// <returns></returns>
-        public static List<YnabAccount> GetAPIYnabAccountsList (ApplicationDbContext context, int personalBudgetID) {
-            List<YnabAccount> accounts = new List<YnabAccount> ();
+        public static List<YnabAccount> GetAPIYnabAccountsList(ApplicationDbContext context, int personalBudgetID)
+        {
+            List<YnabAccount> accounts = new List<YnabAccount>();
 
-            PersonalData personalData = context.PersonalDatas.Where (x => x.ID == personalBudgetID).FirstOrDefault ();
-            JToken jsonAccountsData = GetAPIYnabAccountsJSON (personalData).Result["data"]["accounts"];
+            PersonalData personalData = context.PersonalDatas.Where(x => x.ID == personalBudgetID).FirstOrDefault();
+
+            JToken jsonAccountsData = GetAPIYnabAccountsJSON(personalData).Result["data"]["accounts"];
 
             foreach (JToken jAccount in jsonAccountsData) //item is the account
             {
-                UpdateJAccountValues (jAccount);
-                YnabAccount account = jAccount.ToObject<YnabAccount> ();
+                UpdateJAccountValues(jAccount);
+                YnabAccount account = jAccount.ToObject<YnabAccount>();
                 account.PersonalData = personalData;
-                accounts.Add (account);
+                accounts.Add(account);
             }
 
             return accounts;
@@ -77,11 +95,12 @@ namespace ReactFinancialDashboard.Models {
         /// </summary>
         /// <param name="jAccount"></param>
         /// <returns></returns>
-        public static JToken UpdateJAccountValues (JToken jAccount) {
-            jAccount["balance"] = ((double) jAccount["balance"]) / 1000;
-            jAccount["cleared_balance"] = ((double) jAccount["cleared_balance"]) / 1000;
-            jAccount["uncleared_balance"] = ((double) jAccount["uncleared_balance"]) / 1000;
-            jAccount["type"] = Utilities.SplitStrings.SplitString ((string) jAccount["type"]);
+        public static JToken UpdateJAccountValues(JToken jAccount)
+        {
+            jAccount["balance"] = ((double)jAccount["balance"]) / 1000;
+            jAccount["cleared_balance"] = ((double)jAccount["cleared_balance"]) / 1000;
+            jAccount["uncleared_balance"] = ((double)jAccount["uncleared_balance"]) / 1000;
+            jAccount["type"] = Utilities.SplitStrings.SplitString((string)jAccount["type"]);
             return jAccount;
         }
 
@@ -90,21 +109,30 @@ namespace ReactFinancialDashboard.Models {
         /// </summary>
         /// <param name="context"></param>
         /// <param name="personalBudgetID"></param>
-        public static void UpdateAccountsDatabase (ApplicationDbContext context, int personalBudgetID) {
-            try {
-                using (context) {
+        public static void UpdateAccountsDatabase(ApplicationDbContext context, int personalBudgetID)
+        {
+            try
+            {
+                using (context)
+                {
                     List<YnabAccount> accountsList = GetAPIYnabAccountsList(context, personalBudgetID);
-                    foreach (YnabAccount item in accountsList) {
-                        if (context.YnabAccounts.Contains (item)) {
-                            context.Update (item);
-                        } else {
-                            context.Add (item);
+                    foreach (YnabAccount item in accountsList)
+                    {
+                        if (context.YnabAccounts.Contains(item))
+                        {
+                            context.Update(item);
+                        }
+                        else
+                        {
+                            context.Add(item);
                         }
                     }
-                    context.SaveChanges ();
+                    context.SaveChanges();
                 }
                 // eslint-disable-next-line
-            } catch (Exception) {
+            }
+            catch (Exception)
+            {
                 throw;
             }
         }
