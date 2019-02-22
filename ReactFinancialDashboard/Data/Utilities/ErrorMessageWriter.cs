bd393679f3f4538fbfb0c5a4a8c5eda50d7d2672
Lindsay Alexander <linmalex@gmail.com>
2019-02-22 15:34:37 -0800

    create ErrorMessageWriter to turn Exception into a JObject with the exception message

---
+++ ReactFinancialDashboard/Data/Utilities/ErrorMessageWriter.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Text;
+
+namespace ReactFinancialDashboard.Data.Utilities
+{
+    public class ErrorMessageWriter
+    {
+        public static JObject ExceptionToJObjectWriter(Exception e)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                writer.Formatting = Formatting.Indented;
+                writer.WriteStartObject();
+                writer.WritePropertyName("errorMessage");
+                writer.WriteValue(e.Message);
+                writer.WriteEndObject();
+            }
+            return JObject.Parse(sw.ToString());
+        }
+    }
+}
